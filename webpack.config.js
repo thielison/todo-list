7a993d6b402bc0124e9465eac5c8d1d375203b99
Thielison <67246254+thielison@users.noreply.github.com>
2023-11-13 19:49:09 -0300

    Update package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const path = require("path");
+const HtmlWebpackPlugin = requeri("html-webpack-plugin");
 
 module.exports = {
     mode: "production",
     entry: "./src/index.js",
-    devtool: "inline-source-map",
+    devtool: "source-map",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Todo List App",
+            filename: "index.html",
+            template: "app.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
