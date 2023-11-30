48e93cd977cbd8bb6a8df432e91be4cfac7c867b
Thielison <67246254+thielison@users.noreply.github.com>
2023-11-29 21:46:56 -0300

    Add logic that handles DOM elements and the array of projects/todos

--- src/modules/dom-manager.js
+++ src/modules/dom-manager.js
@@ -1,46 +1,72 @@
+import { projects } from "./project-manager.js";
+
 const addProjectButton = document.getElementById("add-project-button");
 const inputFormForProjectName = document.getElementById("project-name-input-form");
 
-// DOM related functions to create or delete projects
+// Create a new list item that hold the name of the project
+// The data-index attribute set here on each project in the DOM
+// corresponds exactly to the index of the array that holds the Projects
+const createListItemWithProjectName = (projectName, index) => {
+    let li = document.createElement("li");
+    li.textContent = projectName;
+    li.setAttribute("data-index", index);
+    li.classList.add("project");
+
+    return li;
+};
+
+// Function do add an event listener to each project name (list item)
+const addEventListenerToListItem = (li) => {
+    li.addEventListener("click", (e) => {
+        // Check if the clicked element is the delete button
+        if (e.target.className === "delete-btn") {
+            // If it is, remove the project from the DOM
+            removeProjectFromDOM(e.target);
+            // And remove the project from the projects array
+            projects.removeProject(e.target.parentElement.dataset.index);
+            return;
+        }
+    });
+};
+
+// Delete project on each project name on the DOM
 const createDeleteProjectButton = () => {
     const deleteBtn = document.createElement("span");
     deleteBtn.className = "delete-btn";
     return deleteBtn;
 };
 
-const createListItem = (text) => {
-    const li = document.createElement("li");
-    li.textContent = text;
-
-    li.addEventListener("click", (e) => {
-        if (e.target.className === "delete-btn") {
-            deleteProjectFromDOM(e.target);
-        }
-    });
-
-    return li;
-};
-
-const deleteProjectFromDOM = (project) => {
-    project.parentElement.remove();
-};
-
+// Function to append a new project name to the DOM
 const appendProjectNameToDOM = (projectName, index) => {
-    // if (projectName === "") {
-    //     return;
-    // }
-
+    // Select the projects unordered list from the DOM
     const ul = document.querySelector(".projects");
 
-    const li = createListItem(projectName);
-    li.setAttribute("data-index", index);
+    // Create a new list item with project name and data-index
+    const li = createListItemWithProjectName(projectName, index);
 
+    // Add an event listener to the list item
+    addEventListenerToListItem(li);
+
+    // Create a delete button for the project
     const deleteBtn = createDeleteProjectButton();
 
     li.appendChild(deleteBtn);
     ul.append(li);
 
+    // Toggle the visibility of the input for the project name
     toggleHideOrShowInputForProjectName();
+};
+
+const removeProjectFromDOM = (project) => {
+    // Removes a project from the DOM
+    project.parentElement.remove();
+
+    // Updates the data-index attribute of all remaining book cards
+    // to reflect their new position in the projectsManager.projects array
+    let allProjects = document.querySelectorAll(".project");
+    for (let i = 0; i < allProjects.length; i++) {
+        allProjects[i].setAttribute("data-index", i);
+    }
 };
 
 const toggleHideOrShowInputForProjectName = () => {
@@ -54,4 +80,4 @@ const toggleHideOrShowInputForProjectName = () => {
     addProjectButton.style.display = "none";
 };
 
-export { toggleHideOrShowInputForProjectName, appendProjectNameToDOM };
+export { toggleHideOrShowInputForProjectName, appendProjectNameToDOM, removeProjectFromDOM };
--- src/modules/project-manager.js
+++ src/modules/project-manager.js
@@ -1,22 +1,38 @@
 import { appendProjectNameToDOM } from "./dom-manager";
 
+// This function manages an array of projects (an array of to-do items)
+// Each element in the array is an object with an id, projectName, and todos
 const projectsManager = () => {
     const projects = [];
 
     const addNewProject = () => {
         const projectNameInput = document.getElementById("project-name-input");
 
-        let id = projects.length + 1;
+        let id = projects.length;
         let todos = [];
         let projectName = projectNameInput.value;
+
+        if (projectName === "") {
+            return;
+        }
 
         projects.push({ id, projectName, todos });
         appendProjectNameToDOM(projectName, id);
 
         projectNameInput.value = "";
+
+        console.log(projects);
     };
 
-    return { addNewProject };
+    const removeProject = (index) => {
+        if (index > -1) {
+            projects.splice(index, 1);
+        }
+
+        console.log(projects);
+    };
+
+    return { addNewProject, removeProject };
 };
 
 export const projects = projectsManager();
